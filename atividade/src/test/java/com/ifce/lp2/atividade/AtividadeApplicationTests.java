84357fced7760e0542483ecdbc5d83a126c38dbd
jeovaneoliveira31 <jeovaneoliveira31@gmail.com>
2021-03-06 17:54:47 -0300

    iniciando

---
+++ atividade/src/test/java/com/ifce/lp2/atividade/AtividadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifce.lp2.atividade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
