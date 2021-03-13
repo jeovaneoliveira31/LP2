9c5d590d63339e331fe3e8279822d64973f018fc
jeovaneoliveira31 <jeovaneoliveira31@gmail.com>
2021-03-13 05:18:09 -0300

    continuando

--- atividade/src/main/java/com/ifce/lp2/atividade/controller/TodoController.java
+++ atividade/src/main/java/com/ifce/lp2/atividade/controller/TodoController.java
@@ -1,8 +1,12 @@
 package com.ifce.lp2.atividade.controller;
 
+import java.util.List;
+
+import com.ifce.lp2.atividade.model.Todo;
 import com.ifce.lp2.atividade.repository.TodoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +17,15 @@ public class TodoController {
     @Autowired
     private TodoRepository todorepo;
 
-    @GetMapping("/todos")
+    @GetMapping("/Todos")
     public ResponseEntity<?> getAllTodos(){
-      
-    }
 
+        List<Todo> Todos = todorepo.findAll();
+
+        if (Todos.size() > 0) {
+            return new ResponseEntity<List<Todo>>(Todos, HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>("No Todos Avaliable", HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- atividade/src/main/java/com/ifce/lp2/atividade/model/Todo.java
+++ atividade/src/main/java/com/ifce/lp2/atividade/model/Todo.java
@@ -14,6 +14,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Document(collection="Todos")
+
 public class Todo {
 
     @id
